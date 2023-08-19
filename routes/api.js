2601114b1ffc6930d3ba6dc192c2ed1f07f7483b
user <user@MacBook-Pro-user.local>
2023-08-19 22:28:12 +0300

    Prepare

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-const Translator = require('../components/translator.js');
+const Translator = require('../components/translator.js')
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+  const translator = new Translator()
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
-};
+  app.route('/api/translate').post((req, res) => {
+    // {"text":"color","translation":"<span class=\"highlight\">colour</span>"}
+    console.log('OK')
+    res.end()
+  })
+}
