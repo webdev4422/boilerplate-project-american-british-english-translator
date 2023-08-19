d122811306e4e6623f8648a21da5b9c14920523a
user <user@MacBook-Pro-user.local>
2023-08-19 23:13:08 +0300

    Hande some errors

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,21 @@ module.exports = function (app) {
   const translator = new Translator()
 
   app.route('/api/translate').post((req, res) => {
+    const { locale, text } = req.body
+
+    if (!locale || (!locale && !text)) {
+      return res.json({ error: 'Required field(s) missing' })
+    }
+
+    if (locale != 'american-to-british' && locale != 'british-to-american') {
+      return res.json({ error: 'Invalid value for locale field' })
+    }
+
+    if (!text) {
+      return res.json({ error: 'No text to translate' })
+    }
+    // Failed:If text requires no translation, return "Everything looks good to me!" for the translation value.
     // {"text":"color","translation":"<span class=\"highlight\">colour</span>"}
-    console.log('OK')
     res.end()
   })
 }
